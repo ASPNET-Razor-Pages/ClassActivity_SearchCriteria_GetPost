ddc2ad7727a60e7430d7a7a065d74139db2d3318
Zuhair Haroon Khan <zk222ac@gmail.com>
2020-11-10 13:51:24 +0100

    updation

--- ClassActivity_SearchCriteria_GetPost/Pages/Index.cshtml.cs
+++ ClassActivity_SearchCriteria_GetPost/Pages/Index.cshtml.cs
@@ -13,14 +13,19 @@ namespace ClassActivity_SearchCriteria_GetPost.Pages
     public class IndexModel : PageModel
     {
         // Target property for UI 
+        // This property we use when we display collection of objects first time in a Page
         public Dictionary<int, Student> Students { get; set; }
 
         // Target property for Post Method 
+        // This property we use when we make a Post request of form
+        // Adding new Resource information 
         [BindProperty]
         public Student NewStudent { get; set; }
 
         // search criteri property - target property
         [BindProperty(SupportsGet =true)]
+
+        // This property we use when we make a Search of specific objects
         public string SearchCriteria { get; set; }
 
         public FakeRepo Repo;
@@ -29,12 +34,14 @@ namespace ClassActivity_SearchCriteria_GetPost.Pages
             //when we use dependency Injection we dont need to create an FakeRepo object instance in a constructor 
             // repo = new FakeRepo();
             Repo = repo;
+            // Here are we call the GetAllStudents method to retrieve current Dictionary Object List
             Students = Repo.GetAllStudents();
         }
         public IActionResult OnGet()
         {
             if(!String.IsNullOrEmpty(SearchCriteria))
             {
+                // Here we update Students property with current searching criteria
                 Students = Repo.FilterName(SearchCriteria);
             }
             // fill up with data or information
@@ -45,7 +52,9 @@ namespace ClassActivity_SearchCriteria_GetPost.Pages
 
         public IActionResult OnPost()
         {
+            // here we adding new Resource
             Repo.AddNewStudent(NewStudent);
+            // When we added new information , we need to retrieve updated List to show on the screen  
             Students = Repo.GetAllStudents();
             return Page();
 
